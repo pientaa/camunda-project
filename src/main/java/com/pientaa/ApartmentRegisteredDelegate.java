de72e98e3a46e6b73f63fd755b0093c7cd7c9f6d
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-02-11 21:30:36 +0100

    Create delegate poc

---
+++ src/main/java/com/pientaa/ApartmentRegisteredDelegate.java
@@ -0,0 +1,16 @@
+package com.pientaa;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.util.logging.Logger;
+
+public class ApartmentRegisteredDelegate implements JavaDelegate {
+
+    private final static Logger LOGGER = Logger.getLogger("com.pientaa.ApartmentRegisteredDelegate");
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        LOGGER.info("Apartment name: '" + delegateExecution.getVariable("apartmentName") + "'...");
+    }
+}
