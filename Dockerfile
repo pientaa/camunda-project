9624dcaa13dfa8b119e679b676cd4f91b42e5169
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-02-11 18:53:53 +0100

    Register apartment with availability

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ ARG CAMUNDA_IMAGE=camunda/camunda-bpm-platform:wildfly-7.14.0
 
 FROM ${CAMUNDA_IMAGE} AS run
 
-COPY target/online-shopping-1.0-SNAPSHOT.war /camunda/standalone/deployments
+COPY target/aparties-1.0-SNAPSHOT.war /camunda/standalone/deployments
--- src/main/java/com/pientaa/RegisterApartment.java
+++ src/main/java/com/pientaa/RegisterApartment.java
@@ -3,6 +3,6 @@ package com.pientaa;
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
 
-@ProcessApplication("RegisterRoom")
-public class RegisterRoom extends ServletProcessApplication {
+@ProcessApplication("RegisterApartment")
+public class RegisterApartment extends ServletProcessApplication {
 }
