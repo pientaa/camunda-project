4428c1a97fea4f9c78e4f8a2d136213b189c9845
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-02-13 23:46:40 +0100

    Fix git

---
+++ book/src/main/java/com/pientaa/BookApartmentDelegate.java
@@ -0,0 +1,58 @@
+package com.pientaa;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.logging.Logger;
+
+public class BookApartmentDelegate implements JavaDelegate {
+    static final String JDBC_DRIVER = "org.postgresql.Driver";
+    static final String DB_URL = "jdbc:postgresql://postgres:5432/camunda-db";
+    static final String USER = "postgres";
+    static final String PASS = "postgres";
+    private final static Logger LOGGER = Logger.getLogger("com.pientaa.BookApartmentDelegate");
+
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        Connection conn = null;
+        Statement stmt = null;
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+
+            String sql = "select name, rooms, size, description, available_from, duration" +
+                    " from apartment where name = '" + delegateExecution.getVariable("apartmentName") + "'";
+
+            ResultSet rs = stmt.executeQuery(sql);
+            rs.next();
+
+            LOGGER.info(rs.getString("description"));
+
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    conn.close();
+            } catch (SQLException se) {
+                //Do nothing
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+}
+
---
+++ book/src/main/java/com/pientaa/BookingRequest.java
@@ -0,0 +1,18 @@
+package com.pientaa;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BookingRequest implements JavaDelegate {
+    public void execute(DelegateExecution execution) {
+        RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
+        Map<String, Object> processVariables = new HashMap<>();
+        processVariables.put("parentBussinesKey", execution.getProcessInstanceId());
+        processVariables.put("apartmentName", execution.getVariable("apartmentName"));
+        runtimeService.startProcessInstanceByMessage("BookApartment", processVariables);
+    }
+}
